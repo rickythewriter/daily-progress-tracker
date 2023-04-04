6bc2b95d7a1218c305a7b68d9f4b27e8bcfb6f67
Ricky Thang <ricky.writer.dev@gmail.com>
2023-04-04 04:53:57 -0700

    feat - expand student name input to fit row

--- src/components/Student/index.js
+++ src/components/Student/index.js
@@ -20,7 +20,7 @@ export default function Student() {
 
     return (
         <div className='student-form-container'>
-            <div className='form-row student-form-row'>
+            <div id='student-name-row' className='form-row student-form-row'>
                 <label
                     for="name"
                     className='label has-right-margin'>Student:</label>
@@ -31,28 +31,30 @@ export default function Student() {
                     onChange={(e) => setStudentName(e.target.value)}
                 />
             </div>
-            <div className='form-row student-form-row'>
-                <label
-                    for="homework-complete"
-                    className='label has-right-margin'>Homework Complete?</label>
-                <input
-                    name='homework-complete'
-                    type='checkbox'
-                    checked={homeworkComplete}
-                    onChange={(e) => setHomeworkComplete(!homeworkComplete)}
-                />
-            </div>
-            <div className='form-row student-form-row'>
-                <label
-                    for="extra-work-complete"
-                    className='label has-right-margin'>Extra Work Complete?</label>
-                <input
-                    name='extra-work-complete'
-                    type="checkbox"
-                    checked={extraWorkComplete}
-                    onChange={(e) => setExtraWorkComplete(!extraWorkComplete)}
-                >
-                </input>
+            {/* <div className='tablet-row'> */}
+                <div className='form-row student-form-row tablet-column'>
+                    <label
+                        for="homework-complete"
+                        className='label has-right-margin'>Homework Complete?</label>
+                    <input
+                        name='homework-complete'
+                        type='checkbox'
+                        checked={homeworkComplete}
+                        onChange={(e) => setHomeworkComplete(!homeworkComplete)}
+                    />
+                </div>
+                <div className='form-row student-form-row tablet-column'>
+                    <label
+                        for="extra-work-complete"
+                        className='label has-right-margin'>Extra Work Complete?</label>
+                    <input
+                        name='extra-work-complete'
+                        type="checkbox"
+                        checked={extraWorkComplete}
+                        onChange={(e) => setExtraWorkComplete(!extraWorkComplete)}
+                    >
+                    </input>
+                {/* </div> */}
             </div>
             <div className='divider form-row label'>
                 Teacher Comments
